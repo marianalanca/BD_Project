489843d703c06582585b109c32ebc948e1189340
Mariana Luisa Lanca Miguel E Fernandes Marques <mlmarques@student.dei.uc.pt>
2021-04-15 15:26:49 +0100

    kinda register & autentication (the real one)

--- start.py
+++ start.py
@@ -5,6 +5,10 @@ import psycopg2
 from config import config
 
 app = Flask(__name__)
+params = config()
+conn = psycopg2.connect(**params)
+# create a cursor
+cur = conn.cursor()
 
 '''@app.route('/')
 def index():
@@ -14,32 +18,38 @@ def index():
 def hello_name(name):
     return "Hello {}!".format(name)'''
 
+def insert_auction_user(username, password):
+    try :
+        sql = """ insert into auction_user VALUES (%s, %s)"""
+        cur.execute(sql, (username, password))
+        conn.commit()
+        #print(cur.fetchone()[0])
+    except:
+        print('Could not insert new user')
+    return
+
+def autentication_auction_user(username, password):
+    #try :
+    sql = """ SELECT username, password  FROM auction_user where username=%s"""
+    cur.execute(sql, (username,))
+    try:
+        if password!=cur.fetchone()[1]:
+            print('Wrong password')
+    except:
+       print('Wrong username')
+    return
 if __name__ == '__main__':
     # passar para database.ini
     try :
-        params = config()
-        #conn = psycopg2.connect(**params)
         print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params) # aceder à BD
 
-        # create a cursor
-        cur = conn.cursor()
-
-        # execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
-
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
+        # insert_auction_user('nov1o', '1213')
+        autentication_auction_user('marina','pass')
 
 	# close the communication with the PostgreSQL
         cur.close()
 
-
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
 
-    # criar BD!
-
-    app.run()
+    #app.run()
