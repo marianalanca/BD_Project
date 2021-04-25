6caccb6fed3594472403058cf857fcb73c590327
marianalanca <mariana-lanca@hotmail.com>
2021-04-25 15:03:02 +0100

    auth_token

--- python/app/auctionAPI.py
+++ python/app/auctionAPI.py
@@ -1,34 +1,48 @@
+# POSTGRESQL & PSYCOPG2
 # https://www.postgresqltutorial.com/postgresql-python/
 # https://www.postgresql.org/docs/9.2/sql-insert.html
-# https://pyjwt.readthedocs.io/en/stable/
-# https://flask.palletsprojects.com/en/1.1.x/quickstart/
 # https://www.psycopg.org/docs/usage.html
+
+#FLASK
+# https://flask.palletsprojects.com/en/1.1.x/quickstart/
+
+# JWT & AUTHENTICATION
+# https://pyjwt.readthedocs.io/en/stable/
+# https://dev.to/aminu_israel/using-json-web-token-jwt-with-python-3n4p
+# https://steelkiwi.com/blog/jwt-authorization-python-part-1-practise/
 
 
 from flask import Flask, request, redirect, jsonify, session
 import psycopg2, logging
 from config import config
-from datetime import datetime
+import datetime
 import jwt
 
+# INITIALIZATIONS
 app = Flask(__name__)
 app.debug = True
+app.secret_key = 'key'
 params = config()
-userIDs =  {}
+JWT_SECRET = 'secret'
+JWT_ALGORITHM = 'HS256'
+#JWT_EXP_DELTA_SECONDS = 20
 
+# SQL COMMANDS
 INSERT_USER = """ INSERT INTO auction_user VALUES (%s, %s) """
 SELECT_AUCTIONS = " SELECT id, description FROM auction "
 INSERT_AUCTION = " INSERT INTO auction(title, description, id, biddding) VALUES (%s, %s, %s, %.2f) "
+SELECT_USER = """ SELECT username, password  FROM auction_user where username=%s"""
 
-# request.get_json() -> GET DATA
+# FLASK METHODS
 
 @app.route('/user', methods=['POST', 'PUT'])
 def user():
-    if request.method == 'POST': # {“username”: username, “password”:password} -> mudar para também receber mail
-        return insert_auction_user(**request.get_json())
-    else:
-        return autentication_auction_user(**request.get_json())
-    return
+    if(request.get_json()!=None):
+        if request.method == 'POST': # {“username”: username, “password”:password} -> mudar para também receber mail
+            return insert_auction_user(**request.get_json())
+        else:
+            return autentication_auction_user(**request.get_json()) # fazer verificação
+    return {"bye": "bye"}
 
 @app.route('/leilao', methods=['POST'])
 def leilao():
@@ -58,6 +72,10 @@ def leiloes():
 def licitar(leilaoId, licitacao):
     return f'leilao {leilaoId}'
 
+
+# FUNCTIONS
+
+# ? done
 def insert_auction_user(username, password):
     try :
         conn = db_connection()
@@ -65,28 +83,42 @@ def insert_auction_user(username, password):
         cur = conn.cursor()
         cur.execute(INSERT_USER, (username, password,))
         conn.commit()
-        token = str(len(userIDs)) # está mal -> ver melhor
-        #userIDs[token] = username
+        new_id = cur.fetchone()[0]
         cur.close()
-        return {"userId": str(token)}
+        return {"userId": new_id}
     except Exception as e:
         return {"erro": '{m}'.format(m = str(e))}
 
+# TODO
 def autentication_auction_user(username, password):
+    if match_password(username, password):
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=90),
+            'iat': datetime.datetime.utcnow(),
+            'sub': username
+        }
+        encoded = jwt.encode(
+            payload,
+            JWT_SECRET,
+            algorithm=JWT_ALGORITHM
+        )
+        session['authToken'] = encoded
+        return {"auth": encoded}
+    return {"error": "Wrong data"}
+
+def match_password(username, password):
     conn = db_connection()
-    # create a cursor
     cur = conn.cursor()
-    sql = """ SELECT username, password  FROM auction_user where username=%s"""
-    cur.execute(sql, (username,))
+    cur.execute(SELECT_USER, (username,))
     try:
         if password!=cur.fetchone()[1]:
             cur.close()
-            return 'Wrong password'
+            return False
     except:
         cur.close()
-        return 'Wrong username'
+        return False
     cur.close()
-    return "Successful authentication"
+    return True
 
 def create_auction(artigoId, precoMinimo, titulo, descricao, data_de_fim):
 
@@ -113,10 +145,12 @@ def create_auction(artigoId, precoMinimo, titulo, descricao, data_de_fim):
     except Exception as e:
         return {"erro": '{m}'.format(m=str(e))}
 
+# * DONE
 def db_connection():
     db = psycopg2.connect(**params)
     return db
 
+# * DONE
 if __name__ == "__main__":
 
     # Set up the logging
